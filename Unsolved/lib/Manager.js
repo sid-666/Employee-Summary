2a5a6bb8bf2014a15fb6fcba5fc918036f7695fe
Seance <siddharth.dhananjay@gmail.com>
2020-10-17 17:19:51 +1100

    Defined Manager class, inherits from employee

--- Unsolved/lib/Manager.js
+++ Unsolved/lib/Manager.js
@@ -1,1 +1,12 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("./Employee")
+class Manager extends Employee{
+    constructor(name, id, email, officenumber){
+        this.officenumber = officenumber
+        super(name, id, email)
+    }
+    getRole(){
+        return 'Manager'
+    }
+}
+module.exports = Manager
