c55db239eea04b41f796587df0c2ff9abe612779
Seance <siddharth.dhananjay@gmail.com>
2020-10-17 17:19:29 +1100

    Defined Intern class with properties and methods, inherits from Employee

--- Unsolved/lib/Intern.js
+++ Unsolved/lib/Intern.js
@@ -1,1 +1,15 @@
-// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee
+const Employee = require("./Employee")
+class Intern extends Employee{
+    constructor(name, id, email, school){
+        this.school = school
+        super(name, id, email)
+    }
+    getSchool(){
+        return this.school
+    };
+    getRole(){
+        return 'Intern'
+    }
+}
+module.export = Intern
