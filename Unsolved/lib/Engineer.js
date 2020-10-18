698a503bd0968242650f62dc3b9e5f1c626e6ce2
Seance <siddharth.dhananjay@gmail.com>
2020-10-18 18:24:52 +1100

    Fixed engineer class

--- Unsolved/lib/Engineer.js
+++ Unsolved/lib/Engineer.js
@@ -1,15 +1,15 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
 const Employee = require("./Employee")
 class Engineer extends Employee {
-    constructor(name, id, email, github){
+    constructor(name, id, email, github) {
         super(name, id, email)
         this.github = github
     }
-    getGitHub(){
+    getGithub() {
         return this.github
     };
-    getRole(){
+    getRole() {
         return 'Engineer'
     }
 }
-module.export = Engineer
+module.exports = Engineer
