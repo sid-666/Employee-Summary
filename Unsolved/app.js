68aef6664fda6b9e7c6d0fce110c5017d9880a19
Seance <siddharth.dhananjay@gmail.com>
2020-10-18 18:26:45 +1100

    Finished all functions to render html page

--- Unsolved/app.js
+++ Unsolved/app.js
@@ -13,123 +13,129 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+// Question array
 const Questions = [
     {
         type: "input",
         message: "What's the team-members name?",
+        validate: async (input) => {
+            if (!input) return 'Enter response please!'
+            return true
+        },
         name: "name"
     },
     {
         type: "checkbox",
         message: "Whats their role?",
         choices: ["Manager", "Engineer", "Intern"],
+        validate: async (input) => {
+            if (input.length < 1) return 'Enter response please!'
+            return true
+        },
         name: "role"
     },
     {
         type: "input",
         message: "What is their id?",
+        validate: async (input) => {
+            if (!input) return 'Enter response please!'
+            return true
+        },
         name: "id"
     },
     {
         type: "input",
         message: "What is their email? ",
+        validate: async (input) => {
+            if (!input) return 'Enter response please!'
+            return true
+        },
         name: "email"
     },
     {
         type: "input",
         message: "What is their gitHub link?",
+        validate: async (input) => {
+            if (!input) return 'Enter response please!'
+            return true
+        },
         when: (answers) => answers.role == "Engineer",
         name: "github"
     },
     {
         type: "input",
         message: "What school did they attend?",
+        validate: async (input) => {
+            if (!input) return 'Enter response please!'
+            return true
+        },
         when: (answers) => answers.role == "Intern",
         name: "school"
     },
     {
         type: "input",
         message: "What is their officenumber?",
+        validate: async (input) => {
+            if (!input) return 'Enter response please!'
+            return true
+        },
         when: (answers) => answers.role == "Manager",
         name: "officenumber"
     }
 ]
-// const generateEmployeeList = () => {
-//     inquirer.prompt({
-//         type: "input",
-//         message: "How many memebers in team?",
-//         name: "teamnum"
-//     }).then(answer => {
-//         for (var i = 0; i < parseInt(answer.teamnum) + 1; i++) {
-//             inquirer.prompt(Questions)
-//                 .then(answers => {
-// const Employeelist = [];
-// switch (answers) {
-//     case answers.role == "Engineer":
-//         window[`teammember${i + 1}`] = new Engineer(answers.name, answers.id, answers.email, answers.github);
-//         Employeelist.push(window[`teammember${i + 1}`])
-//         break;
-//     case answers.role == "Manager":
-//         window[`teammember${i + 1}`] = new Manager(answers.name, answers.id, answers.email, answers.officenumber);
-//         Employeelist.push(window[`teammember${i + 1}`])
-//         break;
-//     case answers.role == "Intern":
-//         window[`teammember${i + 1}`] = new Intern(answers.name, answers.is, answers.email, answers.school);
-//         Employeelist.push(window[`teammember${i + 1}`])
-//         break;
-// }
-// console.log(Employeelist)
-//                 })
-//         }
-//     })
-// }
+// Async function to convert response from user to a usable team member list that can be rendered to html
 async function generateEmployeeList() {
+    // prompts user for team capacity
     var { teamnum } = await inquirer.prompt({
         type: "input",
         message: "How many members in team?",
+        validate: async (input) => {
+            if (!input) return 'Enter response please!'
+            return true
+        },
         name: "teamnum"
     })
+    const Employeelist = [];
     for (var i = 0; i < parseInt(teamnum); i++) {
         var answers = await inquirer.prompt(Questions)
-        const Employeelist = [];
         // window[`teammember${i + 1}`]
         console.log(answers)
+        // Checks team members role and if so creates a new instance that will be stored in an array
         switch (answers.role[0]) {
-            case answers.role[0] == "Engineer":
+            case "Engineer":
                 var man1 = new Engineer(answers.name, answers.id, answers.email, answers.github);
                 Employeelist.push(man1)
                 break;
-            case answers.role[0] == 'Manager':
+            case "Manager":
                 var man1 = new Manager(answers.name, answers.id, answers.email, answers.officenumber);
                 Employeelist.push(man1)
                 break;
-            case answers.role[0] == "Intern":
-                var man1 = new Intern(answers.name, answers.is, answers.email, answers.school);
+            case "Intern":
+                var man1 = new Intern(answers.name, answers.id, answers.email, answers.school);
                 Employeelist.push(man1)
                 break;
             default:
                 console.log("Whoops");
         }
-        console.log(Employeelist)
+        // console.log(Employeelist)
     }
+    console.log(Employeelist)
+    return Employeelist
 }
+// Gets html content
+async function getHtml() {
+    const Employeearray = await generateEmployeeList();
+    const html = render(Employeearray)
+    console.log(html)
+    return html
+}
+// Writes html content in team.html
+async function postToteamHtml() {
+    var html = await getHtml();
+    fs.writeFile(outputPath, html, function (err) {
+        if (err) throw err;
+        console.log('it worked')
+    })
+}
+postToteamHtml()
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
